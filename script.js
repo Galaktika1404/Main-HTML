5a788515bf073cd2cd010535c6a6b38371469a92
Galaktika1404 <galinatoguleva@gmail.com>
2018-08-01 00:35:44 -0700

    buttons / and - only

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
+var p1 = 0;
+var p2 = 0;
+var operation = 0;
+
 function toInput(id) {
-    let value = document.getElementById(id).value;
-   write(value);
+    write(document.getElementById(id).value);
 }
 
-function write (symbol) {
+function write(symbol) {
     document.getElementById('result').value += symbol;
 }
 
-function subtract(){
-    const a = document.getElementById('').value;
-    const b = document.getElementById('').value;
-    return a-b;
+function subtract() {
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "-";
+    console.log(p1);
+    console.log(operation);
 }
 
 function clearValue() {
-    write('');
+    document.getElementById('result').value = '';
+}
+
+function divide() {
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "/";
+    console.log(p1);
+    console.log(operation);
+}
+
+function getResult() {
+
+    let itogo;
+    p2 = document.getElementById('result').value;
+    console.log(p2);
+    if (operation ==="/"){
+    itogo = p1 / p2;}
+//itogo = p1 - p2;
+    clearValue();
+    write(itogo);
 }
