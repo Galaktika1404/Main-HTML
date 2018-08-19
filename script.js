cafdbe3ce81688c32ebfef656c82d105d2141ed0
Sergey_Togulev <sergtogul@gmail.com>
2018-08-19 00:28:25 -0700

    all buttons. write function

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
+var p1 = 0;
+var p2 = 0;
+var operation = 0;
+
 function toInput(id) {
-    let value = document.getElementById(id).value;
-   write(value);
+    write(document.getElementById(id).value);
 }
 
-function write (symbol) {
+function write(symbol) {
     document.getElementById('result').value += symbol;
 }
 
-function subtract(){
-    const a = document.getElementById('').value;
-    const b = document.getElementById('').value;
-    return a-b;
+function subtract() {
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "-";
+    console.log(p1);
+    console.log(operation);
 }
 
 function clearValue() {
-    write('');
+    document.getElementById('result').value = '';
+}
+
+function divide() {
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "/";
+    console.log(p1);
+    console.log(operation);
+}
+function addNum (){
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "+";
+
+}
+function multiply (){
+    p1 = document.getElementById('result').value;
+    clearValue();
+    operation = "*";
+
+}
+
+function getResult() {
+
+    let itogo;
+    p2 = document.getElementById('result').value;
+
+// logika
+    if (operation === "/") {
+        itogo = p1 / p2;
+    }
+    else if (operation === "+") {
+        itogo = Number(p1) + Number(p2);
+
+    }
+    else if (operation === "-") {
+        itogo = p1 - p2;
+    }
+    else if(operation === "*" ) {
+        itogo = p1 * p2;
+    }
+
+    // end logika
+
+
+    clearValue();
+    write(itogo);
+
 }
